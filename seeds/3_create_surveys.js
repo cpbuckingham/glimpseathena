8d68c17d0f33e2d58bbda923cdef8667273c84a1
cpbuckingham <cameron.p.buckingham@gmail.com>
2017-04-11 17:38:53 -0500

    more seed data for surveys

--- seeds/3_create_surveys.js
+++ seeds/3_create_surveys.js
@@ -4,6 +4,7 @@ exports.seed = function ( knex, Promise ) {
             return Promise.all( [
               //password = test
                 knex( 'surveys' ).insert( {
+                    id: 1,
                     user_id: 1,
                     type: "Office Entry Survey",
                     status_on: true,
@@ -15,15 +16,40 @@ exports.seed = function ( knex, Promise ) {
                     question_5: 'Where did you get your prescription?',
                 } ),
                 knex( 'surveys' ).insert( {
+                    id: 2,
                     user_id: 1,
                     type: "Office Exit Survey",
-                    status_on: false,
-                    title: "goodbye have a great day",
+                    status_on: true,
+                    title: "Hope the office visit went well",
                     question_1: 'How did your appointment go?',
                     question_2: 'Did you feel comfident with your prognosis?',
                     question_3: 'Did you get another prescription?',
                     question_4: 'How old are you?',
                     question_5: 'Do you feel supported?',
+                } ),
+                knex( 'surveys' ).insert( {
+                    id: 3,
+                    user_id: 1,
+                    type: "Pharmacy Entry Survey",
+                    status_on: true,
+                    title: "welcome to the pharmacy",
+                    question_1: 'Are you picking up your prescriptions now?',
+                    question_2: 'Is this the first pharmacy you have visited to pick up?',
+                    question_3: 'Is this your local pharmacy?',
+                    question_4: 'Do you always come here?',
+                    question_5: 'Are you picking up any groceries too?',
+                } ),
+                knex( 'surveys' ).insert( {
+                    id: 4,
+                    user_id: 1,
+                    type: "Pharmacy Exit Survey",
+                    status_on: true,
+                    title: "Hope the pharmacy visit went well",
+                    question_1: 'Did you get the drugs?',
+                    question_2: 'What color are they?',
+                    question_3: 'Are they shinny?',
+                    question_4: 'Did you take the pills already?',
+                    question_5: 'Did you take the green pill or the red pill?',
                 } )
             ] );
         } );
